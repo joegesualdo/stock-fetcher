29079ff0e4206ddb588da73456ef0803d8d8c802
Joe Gesualdo <joegesualdo@gmail.com>
2015-08-21 17:53:26 -0400

    Add error handling

--- index.js
+++ index.js
@@ -6,10 +6,14 @@ function parseBody(response){
 
 function getPrice(symbol, callback) {
   request("http://dev.markitondemand.com/Api/v2/Quote/jsonp?symbol="+symbol+"&callback=myFunction", function(error, response, body){
+    if(error){
+      callback(error);
+    } else{
       var jsonString = parseBody(body);
       // turn string into JSON object
       var parsed = JSON.parse(jsonString)
-      callback(false, parsed["LastPrice"])
+      callback(null, parsed["LastPrice"])
+    }
   });
 }
 
