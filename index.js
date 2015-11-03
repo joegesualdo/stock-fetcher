9f48c70989110d63d9303bfd6f59af17bd673d2f
Joe Gesualdo <joegesualdo@gmail.com>
2015-11-02 23:22:33 -0500

    Clean code

--- index.js
+++ index.js
@@ -4,17 +4,23 @@ function parseBody(response){
   return response.split("(").pop().split(")")[0];
 }
 
-function getPrice(symbol, callback) {
+function getStockObject(symbol, callback){
   request("http://dev.markitondemand.com/Api/v2/Quote/jsonp?symbol="+symbol+"&callback=myFunction", function(error, response, body){
     if(error){
       callback(error);
     } else{
       var jsonString = parseBody(body);
       // turn string into JSON object
-      var parsed = JSON.parse(jsonString)
-      callback(null, parsed["LastPrice"])
+      var stockObject = JSON.parse(jsonString)
+      callback(null, stockObject)
     }
   });
+}
+
+function getPrice(symbol, callback) {
+  getStockObject(symbol, function(err, stockObject){
+    callback(null, stockObject["LastPrice"])
+  })
 }
 
 exports.getPrice = getPrice;
